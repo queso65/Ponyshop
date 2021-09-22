85ce8b50fa9228d8b20ee435cb0dcc36bb4b5918
queso65 <81798262+queso65@users.noreply.github.com>
2021-09-23 01:05:20 +1000

    login

---
+++ login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta name="viewport" content="width=device-width">
+</head>
+<body >
+<div class="login__center">
+    <div style = 'margin:auto;'> 
+        <form action = "">
+            <div class="registration__container ">
+                <h1>Авторизация</h1>
+                <hr class="registration__line">
+
+                <label for="email"><b class="registration__point">Email</b></label>
+                <input type="text" placeholder="Введите Email" name="email" required class="registration__text">
+
+                <label for="psw"><b class="registration__point">Пароль</b></label>
+                <input type="password" placeholder="Введите пароль" name="psw" required class="registration__text">
+				
+                <hr class="registration__line">
+                <button type="submit" class="registerbtn">Войти</button>
+                <div class="registration__container signin">
+                    <p>Нет аккаунта? <a href="" class= "dodgerblue">Зарегистрироваться</a></p>
+                </div>
+	        </div>
+		</form>
+	</div>
+</div>
+
+</body>
+</html>
