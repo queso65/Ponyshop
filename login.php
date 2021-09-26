2759db93fbde36bc7653e6201b6abba5d8177bbb
queso65 <81798262+queso65@users.noreply.github.com>
2021-09-27 00:38:46 +1000

    Update login.php

--- login.php
+++ login.php
@@ -1,16 +1,20 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <meta name="viewport" content="width=device-width">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<meta name="viewport" content="width=device-width">
 </head>
 <body >
 <div class="login__center">
-    <div style = 'margin:auto;'> 
-        <form action = "">
+    <div id = 'div_you_want_centered' style = 'margin:auto;'> 
+        <form action = "vendor/signin.php" method="post">
             <div class="registration__container ">
-                <h1>Авторизация</h1>
-                <hr class="registration__line">
+                 <h1>Авторизация</h1>
+                 <hr class="registration__line">
 
                 <label for="email"><b class="registration__point">Email</b></label>
                 <input type="text" placeholder="Введите Email" name="email" required class="registration__text">
@@ -19,14 +23,22 @@
                 <input type="password" placeholder="Введите пароль" name="psw" required class="registration__text">
 				
                 <hr class="registration__line">
-                <button type="submit" class="registerbtn">Войти</button>
-                <div class="registration__container signin">
-                    <p>Нет аккаунта? <a href="" class= "dodgerblue">Зарегистрироваться</a></p>
-                </div>
-	        </div>
-		</form>
+               <button type="submit" class="registerbtn" >Войти</button>
+            <div class="registration__container signin">
+                    <p>Нет аккаунта? <a href="registration.php" class= "dodgerblue">Зарегистрироваться</a></p>
+            </div>
+   <?php
+        if ($_SESSION['message']) {
+            echo '<p class="msg"> ' . $_SESSION['message'] . ' </p>';
+        }
+        unset($_SESSION['message']);
+    ?>
+	</div>
+	</form>
 	</div>
 </div>
 
 </body>
 </html>
+
+
