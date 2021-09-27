11c2d7d618b90f50eb20e7477acbce503529fc9e
queso65 <81798262+queso65@users.noreply.github.com>
2021-09-27 23:09:20 +1000

    Add files via upload

---
+++ admin_add_product.php
@@ -0,0 +1,145 @@
+<?php
+     require_once 'vendor/connect.php';
+	 require_once 'functions.php';
+	 include 'catalog.php';
+     session_start();
+?>
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <title>Ponyshop</title>
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta name="viewport" content="width=device-width">
+</head>
+<body>	
+	<header class="header">
+		<div class="container align-items_center">
+			<a href="index.php" class="logo" title="logo">
+				<img src="img/logo.png" alt="Logo">
+			</a>
+			<div class="header-right">
+				<form class="search-form">
+					<input type="text" name="search" value="" placeholder="Поиск" class="search search-input">
+					<button><i class="fa fa-search search-i"></i></button>
+				</form>
+				<div class="cart-informer" >
+					<button class="cart-informer__button" onclick="window.location.href='cart.php'">
+						<span class="cart-informer__count" id="cart_count"><?php echo get_cart_count()?></span>
+						<span class="cart-informer__icon"><i class="fa fa-shopping-cart cart-informer__icon-i"></i></span>
+						<span class="cart-informer__value" id="cart_cost"><?php echo get_cart_cost() ?></span>
+					</button>
+				</div>
+				<div class="cart-informer">
+				    <button class="button_login" onclick="window.location.href='login.php'">
+					    Вход
+					</button>
+				</div>			
+		</div>
+	</header>	
+
+
+	<div class="menu">
+		<div class="container">
+			<div class="catalog">
+				<div class="catalog__wrapper" id = "catalog">
+					<div class="catalog__header "><span>Категории</span><i class="fa fa-bars catalog__header-icon"></i></div>
+					<ul class="catalog__list">
+					<?php
+				    foreach($categories as $category){
+					?>
+						<li class="catalog__item">
+						<?php
+						    if($category['parent'] == 0){
+						?>
+							<a href="index.php?id=<?= $category['id']?>" class="catalog__link">
+								<?=$category['title']?>
+							</a>
+							<?php
+						    foreach($categories as $subcategory){
+						    if($category['id'] == $subcategory['parent']){
+						    ?>
+							<div class="catalog__subcatalog">
+								<a href="index.php?id=<?= $subcategory['id']?>" class="catalog__subcatalog-link"><?=$subcategory['title']?></a>
+							</div>
+							<?php
+							    }
+							}
+                        ?>		
+							<?php
+							}
+						?>					
+						</li>
+                   <?php
+				   }
+                   ?>				   
+					</ul>
+				</div>
+			</div>	
+			<nav class="menu__nav">
+				<a href="/" class="menu__nav-link">Главная</a>
+				<a href="/" class="menu__nav-link">Блог</a>
+			</nav>	
+            <a class="menu__border"></a>				
+		</div>
+	</div>
+<p class="message">успешно добавлен</p>
+<div class = "container">
+<div class="vertical-menu">
+  <a href="admin.php" class="border-top">Таблица товаров</a>
+  <a href="users_table.php">Таблица пользователей</a>
+  <a href="#" class="active">Добавить новый товар</a>
+  <a href="#">Таблица пользователей</a>
+  <a href="#" class="border-bottom">Link 4</a>
+</div>
+<div class = "add_product">
+        <form action = "vendor/create.php" method="post" enctype="multipart/form-data">
+            <div class="add_product_container">
+
+                <label for="email"><b class="add_product_point">Название</b></label>
+                <input type="text" name="title" placeholder="Название товара" required class="add_product_text">
+
+                <label for="psw"><b class="add_product_point">Цена</b></label>
+                <input placeholder="Цена товара" name="price" required class="add_product_text">
+				
+				<label for="psw"><b class="add_product_point">Категория</b></label>
+				<select  name="category"   required class="add_product_text">
+				<?php 
+				    $catigories = get_categories();
+					foreach($catigories as $category){
+				?>
+                    <option value="<?php echo $category['id'] ?>"><?php echo $category['title'] ?></option>
+					 <?php
+					}
+			   ?>
+               </select>
+			   
+			   <label for="psw"><b class="add_product_point">Изображение</b></label>
+	           <input type="file" name="image" class="add_product_text" >
+				
+                <hr class="registration__line">
+               <button type="submit" class="registerbtn add_product_btn" >Добавить</button>
+	</div>
+	</form>
+	</div>
+</div>
+<div class="grey__background">
+<div id="footer">
+    <p >Ponyshop&trade; <?php echo date('Y') ?></p>
+</div>
+</div>
+
+
+
+
+	
+	
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="scripts/menu_catalog.js"></script>
+<script src="scripts/main.js"></script>
+<script src="scripts/personal_area.js"></script>
+</body>
+</html>
