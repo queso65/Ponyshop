f3acc083b2a30221b0add1d0235161321bb7c09d
queso65 <81798262+queso65@users.noreply.github.com>
2021-09-27 21:51:21 +1000

    Update index.php

--- index.php
+++ index.php
@@ -15,37 +15,36 @@
 <body>	
 	<header class="header">
 		<div class="container align-items_center">
-			<a href="index.html" class="logo" title="logo">
+			<a href="index.php" class="logo" title="logo">
 				<img src="img/logo.png" alt="Logo">
 			</a>
 			<div class="header-right">
 				<form class="search-form">
-					<input type="text" name="search" value="" placeholder="Поиск" class="search search-input">
+					<input type="text" name="id" value="" placeholder="Поиск" class="search search-input">
 					<button><i class="fa fa-search search-i"></i></button>
 				</form>
 				<div class="cart-informer" >
-					<button class="cart-informer__button" onclick="">
-						<span class="cart-informer__count">0</span>
+					<button class="cart-informer__button" onclick="window.location.href='admin.php'">
+						<span class="cart-informer__count" id="cart_count"><?php echo get_cart_count()?></span>
 						<span class="cart-informer__icon"><i class="fa fa-shopping-cart cart-informer__icon-i"></i></span>
-						<span class="cart-informer__value" >0 Р</span>
+						<span class="cart-informer__value" id="cart_cost"><?php echo get_cart_cost() ?></span>
 					</button>
 				</div>
 				<div class="cart-informer">
-				    <button class="button_login" onclick="">
+				    <button class="button_login" onclick="window.location.href='login.php'">
 					    Вход
 					</button>
-				</div>				
+				</div>		
 		</div>
 	</header>	
-
-
+	 
 	<div class="menu">
 		<div class="container">
 			<div class="catalog">
-				<div class="catalog__wrapper" id = "catalog" >
+				<div class="catalog__wrapper" id = "catalog">
 					<div class="catalog__header "><span>Категории</span><i class="fa fa-bars catalog__header-icon"></i></div>
 					<ul class="catalog__list">
-							<?php
+					<?php
 				    foreach($categories as $category){
 					?>
 						<li class="catalog__item">
@@ -72,7 +71,7 @@
 						</li>
                    <?php
 				   }
-                   ?>			   
+                   ?>				   
 					</ul>
 				</div>
 			</div>	
@@ -83,71 +82,35 @@
             <a class="menu__border"></a>				
 		</div>
 	</div>
+	
+	
 <h1 class="new__goods">Новинки</h1>
+	
 <div class="product-center">
-  <div class="product-item">
-            <img src= "img/T-shirt.jpg">
+<?php
+	foreach($goods as $good){
+	?>
+   <div class="product-item">
+            <img src="<?=$good['image']?>">
             <div class="product-list">
-            <h3 class="item_title">продукт</h3>
-            <span class="price"><span class="item_price">3</span> ₽</span>
-            <button class="button add_item">В корзину</button>
+            <h3 class="item_title"><?=$good['name']?></h3>
+            <span class="price"><span class="item_price"><?=$good['price']?></span> ₽</span>
+            <button data-id="<? echo $good['id'] ?>" class="button add_item"><?php echo in_cart($good['id'])?></button>
         </div>
   </div>
-  <div class="product-item">
-            <img src= "img/T-shirt.jpg">
-            <div class="product-list">
-            <h3 class="item_title">продукт</h3>
-            <span class="price"><span class="item_price">3</span> ₽</span>
-            <button class="button add_item">В корзину</button>
-        </div>
-  </div>
-  <div class="product-item">
-            <img src= "img/T-shirt.jpg">
-            <div class="product-list">
-            <h3 class="item_title">продукт</h3>
-            <span class="price"><span class="item_price">3</span> ₽</span>
-            <button class="button add_item">В корзину</button>
-        </div>
-  </div>
-  <div class="product-item">
-            <img src= "img/T-shirt.jpg">
-            <div class="product-list">
-            <h3 class="item_title">продукт</h3>
-            <span class="price"><span class="item_price">3</span> ₽</span>
-            <button class="button add_item">В корзину</button>
-        </div>
-  </div>
-  <div class="product-item">
-            <img src= "img/T-shirt.jpg">
-            <div class="product-list">
-            <h3 class="item_title">продукт</h3>
-            <span class="price"><span class="item_price">3</span> ₽</span>
-            <button class="button add_item">В корзину</button>
-        </div>
-  </div>
-  <div class="product-item">
-            <img src= "img/T-shirt.jpg">
-            <div class="product-list">
-            <h3 class="item_title">продукт</h3>
-            <span class="price"><span class="item_price">3</span> ₽</span>
-            <button class="button add_item">В корзину</button>
-        </div>
-  </div>
-  <div class="product-item">
-            <img src= "img/T-shirt.jpg">
-            <div class="product-list">
-            <h3 class="item_title">продукт</h3>
-            <span class="price"><span class="item_price">3</span> ₽</span>
-            <button class="button add_item">В корзину</button>
-        </div>
-  </div>
+  <?php
+	}
+?>
 </div>
 <div class="grey__background">
 <div id="footer">
-    <p class="footer_text">Ponyshop&trade; <?php echo date('Y') ?></p>
+    <p >Ponyshop&trade; <?php echo date('Y') ?></p>
 </div>
 </div>
 
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="scripts/menu_catalog.js"></script>
+<script src="scripts/main.js"></script>
 </body>
 </html>
